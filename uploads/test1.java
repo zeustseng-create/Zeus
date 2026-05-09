4b49b475cc7ddbf31851ceb9b1fba0115566a269
zeustseng-create <zeustseng@gmail.com>
2026-05-09 20:28:40 +0800

    upload test1.java

---
+++ uploads/test1.java
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+			int i;
+					i= (int)1.5;
+						    long j;
+						    	    j= (int)(Math.random()*99)+1;
+							    	    Scanner s= new Scanner(System.in);
+								    	    System.out.println("你叫什麼名字 在下方輸入");
+									    	    String name=s.next();
+										    	    System.out.println("hallo"+name+" 終極密碼要開始了喔");
+											            while (true){
+												                System.out.println("請在下方輸入");
+														            int a=s.nextInt();
+															                if (a==j){
+																	                System.out.println("you win");
+
+																			                break;
+																					            }
+																						                if (a>j){
+																								                System.out.println("too big");
+																										            }
+																											                if (a<j){
+																													                System.out.println("too small");
+																															            }
+																																            }
+																																	            
+																																		    	}
+																																			}
+
